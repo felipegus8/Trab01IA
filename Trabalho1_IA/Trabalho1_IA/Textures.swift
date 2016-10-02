72efc10bd4f326f18505de37a2607e9d56bdeb73
gpmarques <guilhermepmarques@globo.com>
2016-10-01 21:22:34 -0300

    Textures e TileView

---
+++ Trabalho1_IA/Trabalho1_IA/Textures.swift
@@ -0,0 +1,14 @@
+//
+//  Textures.swift
+//  Trabalho1_IA
+//
+//  Created by Guilherme Marques on 01/10/16.
+//  Copyright © 2016 Guilherme Marques. All rights reserved.
+//
+
+import Foundation
+
+let florestaDensaTexture = ""
+let trilhaLimpaTexture = ""
+let trilhaComGalhos = ""
+let loboMauTexture = ""
